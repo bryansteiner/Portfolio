cfafb2e5296ce4811f6024d9986ac0d21d772683
Bryan Steiner <steiner.bs@gmail.com>
2015-03-19 21:02:55 -0500

    Progress

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,4 @@
+// Navbar Smooth Scroll
 $("#navbar a[href^='#']").on('click', function(e) {
 
    // prevent default anchor click behavior
@@ -18,6 +19,7 @@ $("#navbar a[href^='#']").on('click', function(e) {
 
 });
 
+// Main Button Smooth Scroll
 $("#main a[href^='#']").on('click', function(e) {
 
    // prevent default anchor click behavior
@@ -29,7 +31,28 @@ $("#main a[href^='#']").on('click', function(e) {
    // animate
    $('html, body').animate({
        scrollTop: $(hash).offset().top
-     }, 1200, function(){
+     }, 750, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
+
+// Contact Button Smooth Scroll
+$("#contact a[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top
+     }, 750, function(){
 
        // when done, add hash to url
        // (default click behaviour)
