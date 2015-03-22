170611eb3b88ddeb606674c51041696fbca9421d
Bryan Steiner <steiner.bs@gmail.com>
2015-03-21 22:45:10 -0500

    Progress

--- js/custom.js
+++ js/custom.js
@@ -16,8 +16,8 @@ $("#navbar a[href^='#']").on('click', function(e) {
        // (default click behaviour)
        window.location.hash = hash;
      });
-
 });
+// End Navbar Smooth Scroll
 
 // Main Button Smooth Scroll
 $("#main a[href^='#']").on('click', function(e) {
@@ -37,10 +37,52 @@ $("#main a[href^='#']").on('click', function(e) {
        // (default click behaviour)
        window.location.hash = hash;
      });
-
 });
+// End Main Button Smooth Scroll
 
-// Contact Button Smooth Scroll
+// About Button Smooth Scroll
+$("#about a[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top
+     }, 750, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+});
+// End About Button Smooth Scroll
+
+// Portfolio Button Smooth Scroll
+$("#portfolio a[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top
+     }, 750, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+});
+// End Portfolio Button Smooth Scroll
+
+// Bottom Button Smooth Scroll
 $("#bottom a[href^='#']").on('click', function(e) {
 
    // prevent default anchor click behavior
@@ -58,5 +100,8 @@ $("#bottom a[href^='#']").on('click', function(e) {
        // (default click behaviour)
        window.location.hash = hash;
      });
-
 });
+// End Contact Button Smooth Scroll
+
+
+
